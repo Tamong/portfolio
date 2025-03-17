9af1928f0bd12519f2a5e82abc408151dff474fb
Philip Wallis <philiptw115@gmail.com>
2025-03-16 23:05:38 -0500

    add games to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import { type MetadataRoute } from "next";
 import { getBlogPosts } from "@/lib/posts";
+import { games } from "@/lib/games";
 import { metaData } from "@/config";
 
 const BaseUrl = metaData.baseUrl.endsWith("/")
@@ -8,15 +9,23 @@ const BaseUrl = metaData.baseUrl.endsWith("/")
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const allPosts = await getBlogPosts();
-  const posts = allPosts.map((post) => ({
+  const allGames = Object.entries(games).map(([slug]) => ({
+    slug,
+  }));
+  const postRoute = allPosts.map((post) => ({
     url: `${BaseUrl}posts/${post.slug}`,
     lastModified: post.publishedAt?.toISOString().split("T")[0] ?? "",
   }));
 
-  const routes = ["", "posts", "resume"].map((route) => ({
+  const gameRoute = allGames.map((game) => ({
+    url: `${BaseUrl}games/${game.slug}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+
+  const routes = ["", "posts", "games", "resume"].map((route) => ({
     url: `${BaseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...posts, ...routes];
+  return [...postRoute, ...gameRoute, ...routes];
 }
